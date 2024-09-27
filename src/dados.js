07bc36e848063dba4268cda7b9a63a15189912e0
Juliana Ferreira <jurferreira23@gmail.com>
2024-09-26 21:15:31 -0300

    criando o projeto

---
+++ src/dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo : "Autoconhecimento", 
+        descricao: " O autoconhecimento é a chave para desbloquear todo seu potencial. Ao se conectar com sua essência, você descobre um mundo de possibilidades e aprende a viver uma vida mais plena e feliz!",
+        link: "https://www.youtube.com/watch?v=LhLTk8KFcyE",
+        tags: "conhecimento" 
+    },    
+
+    {
+        titulo: "Cuidado pessoal", 
+        descricao: "Cuide de si mesmo! Uma rotina de cuidados pessoais pode aumentar sua autoestima e bem-estar. Experimente novas atividades relaxantes, como um banho quente ou uma máscara facial.",
+        link: "https://www.youtube.com/watch?v=2jjpBI2h2Fc",
+        tags: "autocuidado cuidar de si"
+    },    
+
+    {  
+        titulo: "Afirmações positivas",
+        descricao: "As afirmações positivas podem ser um guia para começear o seu dia com o pé direito, te colocando de bom-humor e bom relacionamento consigo mesma. Experimente mudar seu estado mental praticando afirmações positivas! ",
+        link:  "https://www.youtube.com/watch?v=rj1dBSMtyKs",
+        tags: "afirmação"
+    }
+ ];
+
+
+  
+
+
+
+    
+
+
+ 
